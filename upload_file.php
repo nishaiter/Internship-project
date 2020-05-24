9daeee9fd0ba8556f6ddebb3b7a5d16644281317
nisha kumari <nishajhajsr1@gmail.com>
2020-05-25 00:52:30 +0530

    now it shows list of files in grid view of present directory

--- upload_file.php
+++ upload_file.php
@@ -16,4 +16,15 @@ if(move_uploaded_file($_FILES['file']['tmp_name'], $targetfolder))
 
  }
 
- ?>
+		
+ if ($handle = opendir('.')) {
+    while (false !== ($file = readdir($handle))) {
+      if ($file != "." && $file != "..") {
+        $thelist .= '<li><a href="'.$file.'">'.$file.'</a></li>';
+      }
+    }
+    closedir($handle);
+  }
+?>
+<h1>List of files:</h1>
+<ul><?php echo $thelist; ?></ul>
